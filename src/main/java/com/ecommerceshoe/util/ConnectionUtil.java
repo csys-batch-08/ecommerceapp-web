e1894118b7634c86e4d35ec4236d6270901d19c1
VinothGandhi S <vinothgandhi33@gmail.com>
2022-02-03 15:13:05 +0530

    project updated

---
+++ src/main/java/com/ecommerceshoe/util/ConnectionUtil.java
@@ -0,0 +1,47 @@
+package com.ecommerceshoe.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+	public static Connection getDbconnection() {
+		Connection con = null;
+
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+
+	}
+	public static void close(Connection connection, PreparedStatement preparedStatement, ResultSet resultset
+			) {
+		try {
+			if (resultset != null) {
+				resultset.close();
+			}
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+}
